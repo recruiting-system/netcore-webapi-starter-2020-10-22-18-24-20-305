cc32c4741d43ee3c1982e3579530ea432d5a9856
linwenjun <linwenjun@163.com>
2020-10-22 18:24:31 +0000

    first commit

---
+++ netcore-webapi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace netcore_webapi.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public String Home()
+        {
+            return "Hello web api!";
+        }
+    }
+}
